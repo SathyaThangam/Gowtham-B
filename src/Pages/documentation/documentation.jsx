f4860bc80e78a49f7b38492ba6acc11962043d16
gouthamb9599 <gouthamrahul1095@gmail.com>
2020-03-24 20:35:40 +0530

    login and documentation

--- src/Pages/documentation/documentation.jsx
+++ src/Pages/documentation/documentation.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './documentation.css';
 import Navbar from '../../components/navbar';
+import Sidenav from '../../components/sidenav';
 function Documentation() {
     return (
         <div>
             <div className="about">
                 <div className="navbard">
                     <Navbar></Navbar>
+                </div>
+                <div>
+                    <Sidenav></Sidenav>
                 </div>
             </div>
         </div>)
---
+++ src/components/sidenav.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Sidenav() {
+    return (
+        <div>
+
+        </div>)
+}
+export default Sidenav;
